4c85f8ab223d128b2bced0b35c228141648eafa5
ChrisMols <101338263+ChrisMols@users.noreply.github.com>
2023-05-09 21:02:01 +0200

    First version that failed

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,34 +1,19 @@
-const { Builder, By, until } = require('selenium-webdriver');
-require('geckodriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
-const fileUnderTest = 'file://' + __dirname.replace(/ /g, '%20') + '/../dist/index.html';
-const defaultTimeout = 10000;
-let driver;
-jest.setTimeout(1000 * 60 * 5); // 5 minuter
+describe('Webbsida', () => {
+  let driver;
 
-// Det här körs innan vi kör testerna för att säkerställa att Firefox är igång
-beforeAll(async () => {
-console.log(fileUnderTest);
-    driver = await new Builder().forBrowser('firefox').build();
-    await driver.get(fileUnderTest);
-});
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
 
-// Allra sist avslutar vi Firefox igen
-afterAll(async() => {
+  afterAll(async () => {
     await driver.quit();
-}, defaultTimeout);
+  });
 
-test('The stack should be empty in the beginning', async () => {
-	let stack = await driver.findElement(By.id('top_of_stack')).getText();
-	expect(stack).toEqual("n/a");
+  it('testar att sidan laddas korrekt', async () => {
+    await driver.get('http://127.0.0.1:5500/dist/');
+    await driver.wait(until.titleIs('Example Domain'), 1000);
+  });
 });
 
-describe('Clicking "Pusha till stacken"', () => {
-	it('should open a prompt box', async () => {
-		let push = await driver.findElement(By.id('push'));
-		await push.click();
-		let alert = await driver.switchTo().alert();
-		await alert.sendKeys("Bananer");
-		await alert.accept();
-	});
-});
--- tests/stack.test.js
+++ tests/stack.test.js
@@ -1,19 +1,11 @@
-const stack = require('../src/stack');
+describe('Stack', () => {
+    // testfall kommer att skrivas här
+  });
 
-test('peek on empty stack returns undefined', () => {
-    expect(stack.peek()).toBeUndefined();
-});
-
-test('peek on stack with one element returns that element', () => {
+  test('testar om push() fungerar korrekt', () => {
+    const stack = new Stack();
     stack.push(1);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(1);
-});
-
-test('peek on stack with two or more elements returns the top element', () => {
-    stack.push(1);
-    stack.push("wow");
-    stack.push(42);
-    expect(stack.peek()).toBeDefined();
-    expect(stack.peek()).toBe(42);
-});
+    stack.push(2);
+    expect(stack.size()).toBe(2);
+  });
+  
